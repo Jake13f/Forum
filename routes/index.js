ac4687650490a67a3a4e68cfacb0409938524315
Jacob Feliciani <Jake13f@hotmail.com>
2016-12-22 18:05:05 -0500

    Example query

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
+var knex = require('../utilities/knex.js');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   res.render('index', {
-      title: 'Express'
+   knex('test').select().then((results) => {
+      res.render('index', {
+         title: 'Express',
+         results: results
+      });
    });
+
 });
 
 module.exports = router;
