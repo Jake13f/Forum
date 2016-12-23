6c382bc4479545654d0c1a07ab4298c3859b8c9d
Jacob Feliciani <Jake13f@hotmail.com>
2016-12-22 20:56:41 -0500

    Reordered and moved some files to fix order of operation issues

--- utilities/login.js
+++ utilities/login.js
@@ -5,3 +5,22 @@ module.exports.requireLogin = (req, res, next) => {
 	else
 		next();
 }
+
+module.exports.checkLogin = function (req, res, next) {
+	if (req.session && req.session.user) {
+		knex('users').select().where({
+			username: req.session.user.username
+		}).then((user) => {
+			if (user.length > 0) {
+				req.user = user[0];
+				delete req.user.password; // delete the password from the session
+				req.session.user = user; // refresh the session value
+				res.locals.user = user;
+			}
+			// finishing processing the middleware and run the route
+			next();
+		});
+	} else {
+		next();
+	}
+}
