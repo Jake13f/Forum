db910231e7ed44b9dad87d2154a3e537f2981327
Jacob Feliciani <Jake13f@hotmail.com>
2017-01-01 22:56:32 -0500

    Added data tables to the thread list

--- public/js/login.js
+++ public/js/login.js
@@ -10,10 +10,10 @@ $(function() {
          url: '/login/validate',
          method: 'POST',
          data: credentials,
-         success: (errors) => {
+         success: function (errors) {
             if (errors.length > 0) {
                // Display errors
-               errors.forEach((error) => { // display each error
+               errors.forEach(function (error) { // display each error
                   alertify.logPosition('top center');
                   alertify.error(error);
                });
