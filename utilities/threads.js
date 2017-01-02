762a86c197a8e4e3cb83a8580dff67c6a26dc586
Jacob Feliciani <Jake13f@hotmail.com>
2017-01-01 23:50:46 -0500

    Added posting count to the thread listing

--- utilities/threads.js
+++ utilities/threads.js
@@ -3,8 +3,11 @@ var knex = require('../utilities/knex.js');
 // To retrieve the posts and send them back in a callback function to be used
 // @param cb - callback function sending the desired posts.
 module.exports.getThreads = (cb) => {
-   knex('threads').select()
+   knex('threads').select(['threads.id', 'threads.name'])
+   .count('* as count')
    .where('active', '>', 0) // where active
+   .innerJoin('posts', 'posts.thread_id', 'threads.id')
+   .groupBy('threads.name')
    .catch((error) => { cb(); })
-   .then((posts) => { cb(posts); });
+   .then((threads) => { cb(threads); });
 }
