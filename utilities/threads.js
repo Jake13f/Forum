b90f05bca93e685c20421d668cc60252ca1ae47e
Jacob Feliciani <Jake13f@hotmail.com>
2017-01-02 00:04:51 -0500

    Fixed Count for threads

--- utilities/threads.js
+++ utilities/threads.js
@@ -4,9 +4,9 @@ var knex = require('../utilities/knex.js');
 // @param cb - callback function sending the desired posts.
 module.exports.getThreads = (cb) => {
    knex('threads').select(['threads.id', 'threads.name'])
-   .count('* as count')
+   .count('posts.id as count')
    .where('active', '>', 0) // where active
-   .innerJoin('posts', 'posts.thread_id', 'threads.id')
+   .leftJoin('posts', 'posts.thread_id', 'threads.id') // Grabs all of the threads even if there aren't any posts
    .groupBy('threads.name')
    .catch((error) => { cb(); })
    .then((threads) => { cb(threads); });
